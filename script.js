cc46fa3d644f8d0462988b2b04dcfd92f5843cf9
NiklasIMR <niklas@immoor.info>
2026-02-12 13:45:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ document.getElementById("start").contentEditable = true;
 
 // Zielfeld sperren
 document.getElementById("ziel").contentEditable = false;
+
+
+// Switch Button 
+function switchLocations() {
+    console.log("switched");
+    let temp = document.getElementById("ziel").innerText;
+    document.getElementById("ziel").innerText = document.getElementById("start").innerText;
+    document.getElementById("start").innerText = temp;
+}
+document.getElementById("switch").addEventListener("click", switchLocations);
